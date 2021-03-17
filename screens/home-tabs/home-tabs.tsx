0db62574d091318ae69f5d99b8eaa54462ba7fee
Alex Boicu <alex.c.boicu@gmail.com>
2021-03-17 23:04:56 +0200

    Add icons to home tabs

--- screens/home-tabs/home-tabs.tsx
+++ screens/home-tabs/home-tabs.tsx
@@ -2,8 +2,10 @@ import {
   BottomTabNavigationProp,
   createBottomTabNavigator,
 } from "@react-navigation/bottom-tabs";
+import { AntDesign, FontAwesome5 } from '@expo/vector-icons';
 import React from "react";
 import { RootStackParamList } from "../../App";
+import ThemeColors from "../../common/theme/theme-colors";
 import ProfileScreen from "./profile";
 import ServicesScreen from "./services";
 
@@ -21,9 +23,24 @@ const Tab = createBottomTabNavigator();
 
 const HomeTabs = (props: HomeTabsNavigationProp) => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Services" component={ServicesScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+    <Tab.Navigator
+    initialRouteName="Services"
+    tabBarOptions={{
+      activeTintColor: ThemeColors.primary,
+
+    }}>
+      <Tab.Screen name="Services" component={ServicesScreen}
+      options={{
+        tabBarIcon: ({ color, size, focused }) => (
+          <FontAwesome5 name="star" color={color} size={size} solid={focused} />
+        )
+      }} />
+      <Tab.Screen name="Profile" component={ProfileScreen}
+      options={{
+        tabBarIcon: ({ color, size, focused }) => (
+          <FontAwesome5 name="user" color={color} size={size} solid={focused} />
+        )
+      }} />
     </Tab.Navigator>
   );
 };

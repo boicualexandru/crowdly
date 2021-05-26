680b1637bebd2232d052e20bcf2703dccf75d86d
Alex Boicu <alex.c.boicu@gmail.com>
2021-05-26 09:49:08 +0300

    Add login button to profile screen

--- screens/profile.tsx
+++ screens/profile.tsx
@@ -6,13 +6,17 @@ import {
   HomeTabsNavigationPropChild,
   HomeTabsRoutePropChild,
 } from "@navigation/home-tabs";
+import Button from "@components/button/button";
 
 type ProfileScreenNavigationProp = HomeTabsNavigationPropChild<"Profile">;
 type ProfileScreenRouteProp = HomeTabsRoutePropChild<"Profile">;
 
-const ProfileScreen = () => {
-  const navigation = useNavigation<ProfileScreenNavigationProp>();
+type Props = {
+  navigation: ProfileScreenNavigationProp;
+  route: ProfileScreenRouteProp;
+};
 
+const ProfileScreen = ({ navigation, route }: Props) => {
   const goToCreateNew = () => navigation.push("EditVendor", { });
 
   return (
@@ -21,6 +25,11 @@ const ProfileScreen = () => {
       <TouchableOpacity onPress={goToCreateNew}>
         <Text>CREATE NEW</Text>
       </TouchableOpacity>
+      <Button
+        onPress={() => navigation.navigate("Login")}
+        label="Login"
+        style={{ marginVertical: 16 }}
+      />
     </View>
   );
 };

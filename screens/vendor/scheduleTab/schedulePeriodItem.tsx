1a69a0529e648c765f2a5580152cd7b67ae1e727
Alex Boicu <alex.c.boicu@gmail.com>
2021-06-05 17:46:42 +0300

    Use user details on schedule items

--- screens/vendor/scheduleTab/schedulePeriodItem.tsx
+++ screens/vendor/scheduleTab/schedulePeriodItem.tsx
@@ -1,9 +1,14 @@
-import ThemeColors from '@theme/theme-colors';
-import { ThemeTypography, ThemeTypographyColorStyles } from '@theme/theme-typography';
-import { VendorSchedulePeriod } from 'api/schedulePeriods';
-import moment from 'moment';
-import React, { useCallback } from 'react'
-import { Alert, Pressable, StyleSheet, Text, View } from 'react-native'
+import { getImageUrlByUserId } from "api/helpers/getImage";
+import { VendorSchedulePeriod } from "api/schedulePeriods";
+import moment from "moment";
+import React, { useCallback } from "react";
+import { Alert, Pressable, StyleSheet, Text, View, Image } from "react-native";
+
+import ThemeColors from "@theme/theme-colors";
+import {
+  ThemeTypography,
+  ThemeTypographyColorStyles,
+} from "@theme/theme-typography";
 
 interface Props {
   period: VendorSchedulePeriod;
@@ -48,15 +53,30 @@ const SchedulePeriodItem = ({ period, onDelete }: Props) => {
         <View style={styles.datesDivider} />
         <Text style={styles.date}>{getFormattedDate(period.endDate)}</Text>
       </View>
-      {
-        period.description != null && period.description != '' &&
+      {period.bookedByUser != null ? (
+        <View style={styles.userConatiner}>
+          <Image
+            source={{
+              uri: getImageUrlByUserId(
+                period.bookedByUser.id,
+                period.bookedByUser.image
+              ),
+            }}
+            style={styles.userImage}
+          />
+          <Text style={styles.userName}>
+            {period.bookedByUser?.firstName} {period.bookedByUser?.lastName}
+          </Text>
+        </View>
+      ) : null}
+      {period.description != null && period.description != "" && (
         <Text style={styles.description}>{period.description}</Text>
-      }
+      )}
     </Pressable>
   );
 };
 
-export default SchedulePeriodItem
+export default SchedulePeriodItem;
 
 const styles = StyleSheet.create({
   scheduleItem: {
@@ -88,4 +108,19 @@ const styles = StyleSheet.create({
     ...ThemeTypographyColorStyles.text_dark_60,
     marginTop: 16,
   },
-})
+  userConatiner: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginTop: 16,
+  },
+  userImage: {
+    height: 30,
+    width: 30,
+    borderRadius: 40,
+    marginRight: 8,
+  },
+  userName: {
+    ...ThemeTypography.body2,
+    ...ThemeTypographyColorStyles.text_dark_60,
+  },
+});
